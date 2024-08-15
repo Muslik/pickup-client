fd988623501357fb624a2f79170aa57685edd0f4
Dzhabrail <dzhabulya@gmail.com>
2024-08-15 23:26:18 +0300

    Updated workflow, init fsd

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,9 @@
 import { useState } from 'react';
-import reactLogo from 'src/assets/react.svg';
 
-import './App.css';
+import './App.scss';
 import viteLogo from '/vite.svg';
 
-function App() {
+export function App() {
   const [count, setCount] = useState(0);
 
   return (
@@ -12,9 +11,6 @@ function App() {
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
@@ -28,5 +24,3 @@ function App() {
     </>
   );
 }
-
-export default App;
